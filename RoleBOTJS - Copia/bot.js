cc6d692e63f74d091fa9728ae5a2aeb6f7c09349
crasho25 <43079983+crasho25@users.noreply.github.com>
2018-09-22 16:13:20 +0200

    Update bot.js

--- RoleBOTJS - Copia/bot.js
+++ RoleBOTJS - Copia/bot.js
@@ -6,6 +6,14 @@
 
 const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
 
+static void UpdatePresence()
+    {
+        DiscordRichPresence discordPresence;
+        memset(&discordPresence, 0, sizeof(discordPresence));
+        discordPresence.details = "Scrivi .aiuto";
+        Discord_UpdatePresence(&discordPresence);
+    }
+
 if(!process.send) {
 
 Files.initStandalone();
